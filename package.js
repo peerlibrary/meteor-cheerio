5442b8672f119946c19f15d89dbeac6970813c32
Rob Whitfield <chillmatica@github.com>
2013-08-20 00:48:02 -0700

    Updated to work with Meteor 0.6.5

--- package.js
+++ package.js
@@ -2,9 +2,12 @@ Package.describe({
   summary: "Simple Cheerio NPM wrapper"
 });
 
-Npm.depends({"cheerio": "0.11.0"});
+Npm.depends({"cheerio": "0.12.1"});
 
 Package.on_use(function (api) {
 
   api.add_files(["/lib/cheeriowrap.js"], "server");
+  if (typeof api.export !== 'undefined') {
+    api.export(['cheerio'], 'server');
+  }
 });
