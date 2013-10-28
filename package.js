f7b7b8c4ad4dbac7e9b34a4b110150cd5ec95bae
Andrei Oprisan <andrei@oprisan.com>
2013-10-27 22:10:22 -0400

    0.3.2

--- package.js
+++ package.js
@@ -2,11 +2,11 @@ Package.describe({
   summary: "Simple Cheerio NPM wrapper"
 });
 
-Npm.depends({"cheerio": "0.12.1"});
+Npm.depends({"cheerio": "0.12.3"});
 
 Package.on_use(function (api) {
 
-  api.add_files(["/lib/cheeriowrap.js"], "server");
+  api.add_files(["lib/cheeriowrap.js"], "server");
   if (typeof api.export !== 'undefined') {
     api.export(['cheerio'], 'server');
   }
