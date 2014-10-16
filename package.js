7532ba864171826ebd6096edd80bfd17dec4dbc5
Mitar <mitar.git@tnode.com>
2014-10-16 03:27:28 -0700

    Migrated to Meteor 0.9.0.

--- package.js
+++ package.js
@@ -1,12 +1,15 @@
 Package.describe({
-  summary: "Simple Cheerio NPM wrapper"
+  summary: "Simple Cheerio NPM wrapper",
+  version: '0.17.0_1',
+  git: 'https://github.com/andreioprisan/meteor-cheerio.git'
 });
 
-Npm.depends({"cheerio": "0.17.0"});
+Npm.depends({'cheerio': '0.17.0'});
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.0');
 
-  api.add_files(["lib/cheeriowrap.js"], "server");
+  api.add_files(['lib/cheeriowrap.js'], 'server');
   if (typeof api.export !== 'undefined') {
     api.export(['cheerio'], 'server');
   }
